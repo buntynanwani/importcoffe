8c79f5392250d2ffe420586615ca626e6f89f907
robertosaavedr <robertocarlos.saavedrabaylon@gmail.com>
2025-10-05 12:38:26 +0200

    feature: new endpoint to get current hospitals

--- Backend/code/Backend/urls.py
+++ Backend/code/Backend/urls.py
@@ -1,6 +1,8 @@
 from .views import  get_proposed_medical_centers
+from .views import  get_medical_centers
 from django.urls import path
 
 urlpatterns = [
     path('get_proposed_medical_centers', get_proposed_medical_centers.as_view(), name = "get_proposed_medical_centers"),
+    path('get_medical_centers', get_medical_centers.as_view(), name = "get_medical_centers"),
 ]
